1f328224fc7ee53aa60c620f4f6fbab20b82de65
Shubham Banubakode <shubhambanubakode05@gmail.com>
2026-07-24 11:10:17 +0530

    Add HTTP health route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const http = require("http");
 const { Server } = require("socket.io");
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain",
+  });
+
+  res.end("Anonymous Chat Socket Server is Running 🚀");
+});
 
 const io = new Server(server, {
   cors: {
